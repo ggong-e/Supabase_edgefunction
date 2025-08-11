b0a85d7f9a2696b5d5274f168f0d7f74a54321d8
ggong-e <sungbop@gmail.com>
2025-08-11 18:16:12 +0900

    Update curl_test.ps1

--- curl_test.ps1
+++ curl_test.ps1
@@ -1,9 +1,10 @@
-﻿# 사용법: PowerShell에서 이 파일이 있는 폴더에서 실행
+# 사용법: PowerShell에서 이 파일이 있는 폴더에서 실행
 # .\curl_test.ps1
 
- = '<엔드포인트>'
-     = '{"symbol":"005930","from":"20250101"}'
+$endpoint = 'https://avlvrnxunawsuhekjojt.supabase.co/functions/v1/kis-test'
+$body     = '{"symbol":"005930","from":"20250101"}'
 
-curl.exe -s -X POST  
-  -H "Content-Type: application/json" 
-  -d  | Out-String | Write-Host
+curl.exe -s -X POST $endpoint `
+  -H "Content-Type: application/json" `
+  -d $body | Out-String | Write-Host
+
